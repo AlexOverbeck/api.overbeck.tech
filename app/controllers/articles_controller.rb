084314fb3e4ee25f8155c778628af94f0a16fef9
Alex Overbeck <alexoverbeck@gmail.com>
2021-10-23 22:18:41 -0400

    Add basic article routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+    render json: Article.all
+  end
+
+  def create
+    article = Article.new article_params
+
+    if article.save
+      render json: article
+    end
+  end
+
+  def destroy
+    article = Article.find params[:id]
+
+    if article.destroy
+      render json: article
+    end
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :summary, :url)
+  end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index action renders successfully" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "index action returns all the articles" do
+    article_count = Article.count
+    assert  article_count > 0
+
+    get articles_url
+    assert_response :success
+
+    articles = JSON.parse response.body
+    assert_equal articles.count, article_count
+  end
+
+  test "the create action creates and returns a new article" do
+    article_params = {
+      title: "Rando Title",
+      summary: "Some random summary",
+      url: "https://rando.url.something"
+    }
+
+    assert_difference("Article.count", +1) do
+      post articles_url, params: article_params
+      assert_response :success
+    end
+
+    article = JSON.parse response.body
+
+    assert(article["id"].present?)
+    [:title, :summary, :url].each do |key|
+      assert_equal article[key.to_s], article_params[key]
+    end
+  end
+
+  test "the destroy action deletes and returns an article" do
+    article = Article.last
+    assert_difference("Article.count", -1) do
+      delete article_url article
+      assert_response :success
+    end
+
+    deleted_article = JSON.parse response.body
+
+    assert(deleted_article["id"].present?)
+    [:title, :summary, :url].each do |key|
+      assert_equal deleted_article[key.to_s], article[key]
+    end
+  end
 end
